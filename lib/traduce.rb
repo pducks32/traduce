f7e952e2223085a90375583e2d19399ae4c3900f
Patrick Metcalfe <git@patrickmetcalfe.com>
2015-03-15 21:18:36 -0500

    conjugate active singular present indicative tenses

--- lib/traduce.rb
+++ lib/traduce.rb
@@ -1,5 +1,39 @@
 require "traduce/version"
 
 module Traduce
-  # Your code goes here...
+  def self.conjugate(verb, person:, number: 1, tense: :present, mood: :indictive, voice: :active)
+    verb = Verb.new(verb) unless verb.is_a? Verb
+    verb.gsub(/([e|a|i])r\Z/) do |m|
+      ending = verb.ar? ? "a" : "e"
+      suffix = person.eql?(3) ? "#{ending}" : "#{ending}s"
+      person.eql?(1) ? "o" : suffix
+    end
+  end
+
+  class Verb < SimpleDelegator
+
+    def initialize(verb)
+      super
+      @verb = verb
+    end
+
+    def er?
+      ending == "er"
+    end
+    alias_method :er_verb?, :er?
+
+    def ar?
+      ending == "ar"
+    end
+    alias_method :ar_verb?, :ar?
+
+    def ir?
+      ending == "ir"
+    end
+    alias_method :ir_verb?, :ir?
+
+    def ending
+      @verb[-2, 2]
+    end
+  end
 end
--- spec/traduce_spec.rb
+++ spec/traduce_spec.rb
@@ -1,11 +1,74 @@
 require 'spec_helper'
 
-describe Traduce do
-  it 'has a version number' do
-    expect(Traduce::VERSION).not_to be nil
+RSpec.describe Traduce do
+  context "with the active present indicative tense" do
+    context "in the first person singular" do
+      it "conjugates a regular -ar verb in the first person" do
+        conjugation = Traduce.conjugate("hablar", number: 1, tense: :present, person: 1, mood: :indictive, voice: :active)
+        expect(conjugation).to eql "hablo"
+      end
+      it "conjugates a regular -er verb in the first person active singular tense" do
+        conjugation = Traduce.conjugate("temer", number: 1, tense: :present, person: 1, mood: :indictive, voice: :active)
+        expect(conjugation).to eql "temo"
+      end
+      it "conjugates a regular -ir verb in the first person active singular tense" do
+        conjugation = Traduce.conjugate("partir", number: 1, tense: :present, person: 1, mood: :indictive, voice: :active)
+        expect(conjugation).to eql "parto"
+      end
+    end
+
+    context "in the second person singular" do
+      it "conjugates a regular -ar verb in the second person active singular tense" do
+        conjugation = Traduce.conjugate("hablar", number: 1, tense: :present, person: 2, mood: :indictive, voice: :active)
+        expect(conjugation).to eql "hablas"
+      end
+      it "conjugates a regular -er verb in the second person active singular tense" do
+        conjugation = Traduce.conjugate("temer", number: 1, tense: :present, person: 2, mood: :indictive, voice: :active)
+        expect(conjugation).to eql "temes"
+      end
+      it "conjugates a regular -ir verb in the second person active singular tense" do
+        conjugation = Traduce.conjugate("partir", number: 1, tense: :present, person: 2, mood: :indictive, voice: :active)
+        expect(conjugation).to eql "partes"
+      end
+    end
+
+    context "in the third person singular" do
+      it "conjugates a regular -ar verb in the second person active singular tense" do
+        conjugation = Traduce.conjugate("hablar", person: 3)
+        expect(conjugation).to eql "habla"
+      end
+      it "conjugates a regular -er verb in the second person active singular tense" do
+        conjugation = Traduce.conjugate("temer",person: 3)
+        expect(conjugation).to eql "teme"
+      end
+      it "conjugates a regular -ir verb in the second person active singular tense" do
+        conjugation = Traduce.conjugate("partir", person: 3)
+        expect(conjugation).to eql "parte"
+      end
+    end
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe Traduce::Verb do
+    it "knows its proper ending" do
+      expect(Traduce::Verb.new("hablar").ending).to eql "ar"
+    end
+    context "when -ar verb" do
+      subject { Traduce::Verb.new("hablar") }
+      it { is_expected.to be_ar_verb }
+      it { is_expected.to_not be_ir_verb }
+      it { is_expected.to_not be_er_verb }
+    end
+    context "when -er verb" do
+      subject { Traduce::Verb.new("temer") }
+      it { is_expected.to_not be_ar_verb }
+      it { is_expected.to_not be_ir_verb }
+      it { is_expected.to be_er_verb }
+    end
+    context "when -ir verb" do
+      subject { Traduce::Verb.new("partir") }
+      it { is_expected.to_not be_ar_verb }
+      it { is_expected.to be_ir_verb }
+      it { is_expected.to_not be_er_verb }
+    end
   end
 end
