009951a80f3dc5519b868648810f41f7730e00be
Patrick Metcalfe <git@patrickmetcalfe.com>
2015-03-16 20:25:22 -0500

    add top level integration test

--- spec/traduce_spec.rb
+++ spec/traduce_spec.rb
@@ -1,5 +1,8 @@
 require 'spec_helper'
 
 RSpec.describe Traduce do
+  it "conjugates a verb" do
+    conjugation = Traduce.conjugate("hablar", person: 1)
+    expect(conjugation).to eql "hablo"
   end
 end
