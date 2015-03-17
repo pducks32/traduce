151bec3c574409a83c91adcdc4c841e456d79c01
Patrick Metcalfe <git@patrickmetcalfe.com>
2015-03-16 20:26:22 -0500

    Add top level functionality

---
+++ lib/traduce.rb
@@ -0,0 +1,10 @@
+require "traduce/version"
+require "traduce/verb"
+require "traduce/conjugation"
+
+module Traduce
+  def self.conjugate(verb, **conjugation_options)
+    verb = Verb.new(verb) unless verb.is_a? Verb
+    Conjugation.new(conjugation_options).conjugate(verb)
+  end
+end
