a0e226768790e58a02a1fcdf48847d37e1df5c94
Patrick Metcalfe <git@patrickmetcalfe.com>
2015-03-23 19:30:50 -0500

    fixed kwargs mistake on method invoke

--- lib/traduce.rb
+++ lib/traduce.rb
@@ -5,6 +5,6 @@ require "traduce/conjugation"
 module Traduce
   def self.conjugate(verb, **conjugation_options)
     verb = Verb.new(verb) unless verb.is_a? Verb
-    Conjugation.new(conjugation_options).conjugate(verb)
+    Conjugation.new(**conjugation_options).conjugate(verb)
   end
 end
